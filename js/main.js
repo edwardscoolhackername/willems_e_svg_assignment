6d6cb947547a3484c0d89f23ab5fb899ee6efb60
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-06-25 13:50:25 -0400

    svgs fixed and linked into html

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Identify the elements we want to use //
+let smallfaces = document.querySelectorAll(".iconface"),
+      everybody = document.querySelectorAll(".facebox"),
+      edward = document.querySelector("#edwardbox"),
+      jasmin = document.querySelector("#jasminbox"),
+      nic = document.querySelector("#nicbox"),
+      sam = document.querySelector("#sambox"),
+      thomas = document.querySelector("#thomasbox");
+
+
+everybody.classList.add("hidden");
